117dda2c52f2186abc6c27749c82095ccedb2924
Xiaoxu <xiaoxul@andrew.cmu.edu>
2014-11-19 19:47:03 -0500

    new pipeline for snippet retrieval

--- project-team02/src/main/java/edu/cmu/lti/oaqa/pipeline/SnippetAnnotator.java
+++ project-team02/src/main/java/edu/cmu/lti/oaqa/pipeline/SnippetAnnotator.java
@@ -1,1 +1,15 @@
-must write something...
+package edu.cmu.lti.oaqa.pipeline;
+
+import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.jcas.JCas;
+
+public class SnippetAnnotator extends JCasAnnotator_ImplBase {
+
+	@Override
+	public void process(JCas aJCas) throws AnalysisEngineProcessException {
+		// TODO Auto-generated method stub
+		System.err.println("doing snnipet retrieval!!!!!!!!!!!!!!!!!!!!!!");
+	}
+
+}
