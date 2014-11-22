af714b1523349f3febc83a6cd9140d9a1a895e9e
wangyifu <wyfisgood@gmail.com>
2014-11-22 11:26:33 -0500

    Add snippet on evaluator, separate scores for different types

--- project-team02/src/main/java/edu/cmu/lti/oaqa/pipeline/CasConsumer.java
+++ project-team02/src/main/java/edu/cmu/lti/oaqa/pipeline/CasConsumer.java
@@ -1,6 +1,8 @@
 package edu.cmu.lti.oaqa.pipeline;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -8,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 import json.JsonCollectionReaderHelper;
+import json.gson.Snippet;
 import json.gson.TestQuestion;
 import json.gson.Triple;
 
@@ -24,7 +27,6 @@ import edu.cmu.lti.oaqa.type.retrieval.Document;
 import edu.cmu.lti.oaqa.type.retrieval.TripleSearchResult;
 import edu.cmu.lti.oaqa.type.retrieval.Passage;
 
-
 public class CasConsumer extends CasConsumer_ImplBase {
 
   public static String OUTPUTPATH = "";
@@ -37,6 +39,8 @@ public class CasConsumer extends CasConsumer_ImplBase {
 
   private Map<String, List<Triple>> tripleMap;
 
+  private Map<String, List<Snippet>> snippetMap;
+
   private List<TestQuestion> goldstandards;
 
   private JsonCollectionReaderHelper jsonHelper;
@@ -48,15 +52,21 @@ public class CasConsumer extends CasConsumer_ImplBase {
     OUTPUTPATH = "output.json";
     goldstandards = jsonHelper.testRun();
 
+    // Dictionaries of golden standard answer
     docMap = new HashMap<String, List<String>>();
     conceptMap = new HashMap<String, List<String>>();
     tripleMap = new HashMap<String, List<Triple>>();
+    snippetMap = new HashMap<String, List<Snippet>>();
+
+    // get golden answer
     for (int i = 0; i < goldstandards.size(); i++) {
       docMap.put(goldstandards.get(i).getId(), goldstandards.get(i).getDocuments());
       conceptMap.put(goldstandards.get(i).getId(), goldstandards.get(i).getConcepts());
       tripleMap.put(goldstandards.get(i).getId(), goldstandards.get(i).getTriples());
+      snippetMap.put(goldstandards.get(i).getId(), goldstandards.get(i).getSnippets());
     }
 
+    // initialize evaluator
     evaluator = new Evaluator();
   }
 
@@ -69,39 +79,44 @@ public class CasConsumer extends CasConsumer_ImplBase {
       e.printStackTrace();
     }
     String qid = TypeUtil.getQuestion(jcas).getId();
-    
-    //create empty list if no list returned, to avoid null pointer exception
-    Collection<Document> docList = TypeUtil.getRankedDocuments(jcas) == null ? new ArrayList<Document>()
+
+    // create empty list if no list returned, to avoid null pointer exception
+    Collection<Document> docList = (TypeUtil.getRankedDocuments(jcas) == null) ? new ArrayList<Document>()
             : TypeUtil.getRankedDocuments(jcas);
-    Collection<ConceptSearchResult> conceptList = TypeUtil.getRankedConceptSearchResults(jcas) == null ? new ArrayList<ConceptSearchResult>()
+    Collection<ConceptSearchResult> conceptList = (TypeUtil.getRankedConceptSearchResults(jcas) == null) ? new ArrayList<ConceptSearchResult>()
             : TypeUtil.getRankedConceptSearchResults(jcas);
-    Collection<TripleSearchResult> tripleList = TypeUtil.getRankedTripleSearchResults(jcas) == null ? new ArrayList<TripleSearchResult>()
+    Collection<TripleSearchResult> tripleList = (TypeUtil.getRankedTripleSearchResults(jcas) == null) ? new ArrayList<TripleSearchResult>()
             : TypeUtil.getRankedTripleSearchResults(jcas);
-    
-    
-    Collection<Passage> SnippetList = TypeUtil.getRankedPassages(jcas) == null ? new ArrayList<Passage>()
+    Collection<Passage> snippetList = (TypeUtil.getRankedPassages(jcas) == null) ? new ArrayList<Passage>()
             : TypeUtil.getRankedPassages(jcas);
 
-    List<String> docGold = docMap.get(qid) == null ? new ArrayList<String>() : docMap.get(qid);
-    List<String> conceptGold = conceptMap.get(qid) == null ? new ArrayList<String>() : conceptMap
+    // create golden standard answer list for evaluator
+    List<String> docGold = (docMap.get(qid) == null) ? new ArrayList<String>() : docMap.get(qid);
+    List<String> conceptGold = (conceptMap.get(qid) == null) ? new ArrayList<String>() : conceptMap
             .get(qid);
-    List<Triple> tripleGold = tripleMap.get(qid) == null ? new ArrayList<Triple>() : tripleMap
+    List<Triple> tripleGold = (tripleMap.get(qid) == null) ? new ArrayList<Triple>() : tripleMap
             .get(qid);
+    List<Snippet> snippetGold = (snippetMap.get(qid) == null) ? new ArrayList<Snippet>()
+            : snippetMap.get(qid);
 
-    System.out.println("------------------------------------------");
+    System.out.println("+-----------------------------------------");
     System.out.println("| Evaluatation");
-    System.out.println("|-----------document----------------------");
+    System.out.println("+-----------document----------------------");
     System.out.println("| doc ans size: " + docList.size() + " | gold ans size:" + docGold.size());
     System.out.println("| postive: " + evaluator.calDocPositive(docList, docGold));
-    System.out.println("|-----------concept----------------------");
+    System.out.println("+-----------concept----------------------");
     System.out.println("| concept ans size: " + conceptList.size() + " | gold ans size:"
             + conceptGold.size());
     System.out.println("| postive: " + evaluator.calConceptPositive(conceptList, conceptGold));
-    System.out.println("|-----------triple----------------------");
+    System.out.println("+-----------triple----------------------");
     System.out.println("| triple ans size: " + tripleList.size() + " | gold ans size:"
             + tripleGold.size());
     System.out.println("| postive: " + evaluator.calTriplePositive(tripleList, tripleGold));
-    System.out.println("------------------------------------------");
+    System.out.println("+-----------snippet----------------------");
+    System.out.println("| snippet ans size: " + snippetList.size() + " | gold ans size:"
+            + snippetGold.size());
+    System.out.println("| postive: " + evaluator.calSnippetPositive(snippetList, snippetGold));
+    System.out.println("+-----------------------------------------");
 
     // print out results
     /*
@@ -110,43 +125,59 @@ public class CasConsumer extends CasConsumer_ImplBase {
      * System.out.println(concept.getText()+" score:"+concept.getScore()); } for (TripleSearchResult
      * triple : tripleList){ System.out.println(triple.getText()+" score:"+triple.getScore()); }
      */
-    
-    
-   
-int count=0;
-    for (Passage snippet : SnippetList){ 
-    	if(count==5) break;
-  	  
-  	  System.out.println("beginSection:" + snippet.getBeginSection());
-  	 System.out.println("document:" + snippet.getUri());
-  	 System.out.println("endSection:" + snippet.getEndSection());
-  	 System.out.println("offsetInBeginSection:" + snippet.getOffsetInBeginSection());
-  	 System.out.println("offsetInEndSection:" + snippet.getOffsetInEndSection());
-  	 System.out.println("text:" + snippet.getText());
-  	 System.out.println("Score:" + snippet.getScore());
-  	 count++;
-  	  }
-     
 
-  
+    /*
+     * int count = 0; for (Passage snippet : snippetList) { if (count == 5) break;
+     * 
+     * System.out.println("beginSection:" + snippet.getBeginSection());
+     * System.out.println("document:" + snippet.getUri()); System.out.println("endSection:" +
+     * snippet.getEndSection()); System.out.println("offsetInBeginSection:" +
+     * snippet.getOffsetInBeginSection()); System.out.println("offsetInEndSection:" +
+     * snippet.getOffsetInEndSection()); System.out.println("text:" + snippet.getText());
+     * System.out.println("Score:" + snippet.getScore()); count++; }
+     */
 
   }
 
   /**
-   * This method is called after all objects are processed. We calculate precision, recall F-score
-   * here
+   * This method is called after all objects are processed. We print precision, recall, F-score,
+   * MAP, and GMAP here
    */
   @Override
   public void collectionProcessComplete(ProcessTrace arg0) throws ResourceProcessException,
           IOException {
-    System.out.println("-------------------------------------");
-    System.out.println("| Performance Report");
-    System.out.println("| Precision: " + evaluator.getPrecision());
-    System.out.println("| Recall: " + evaluator.getRecall());
-    System.out.println("| F-Score: " + evaluator.getFScore());
-    System.out.println("| Mean Average Precison: " + evaluator.getMAP());
-    System.out.println("| Geometric Mean Average Precison: " + evaluator.getGMAP());
-    System.out.println("-------------------------------------");
+    DecimalFormat df = new DecimalFormat("0.0000");
+    System.out.println("+-------------------------------------");
+    System.out.println("|       Performance Report           |");
+    System.out.println("+------------------------------------+");
+    System.out.println(" Document                           ");
+    System.out.println(" Precision: " + df.format(evaluator.getPrecision("document")));
+    System.out.println(" Recall: " + df.format(evaluator.getRecall("document")));
+    System.out.println(" F-Score: " + df.format(evaluator.getFScore("document")));
+    System.out.println(" MAP: " + df.format(evaluator.getMAP("document")));
+    System.out.println(" GMAP: " + df.format(evaluator.getGMAP("document")));
+    System.out.println("+------------------------------------+");
+    System.out.println("| Concept                            ");
+    System.out.println("| Precision: " + df.format(evaluator.getPrecision("concept")));
+    System.out.println("| Recall: " + df.format(evaluator.getRecall("concept")));
+    System.out.println("| F-Score: " + df.format(evaluator.getFScore("concept")));
+    System.out.println("| MAP: " + df.format(evaluator.getMAP("concept")));
+    System.out.println("| GMAP: " + df.format(evaluator.getGMAP("concept")));
+    System.out.println("+------------------------------------+");
+    System.out.println("| Triple                             ");
+    System.out.println("| Precision: " + df.format(evaluator.getPrecision("triple")));
+    System.out.println("| Recall: " + df.format(evaluator.getRecall("triple")));
+    System.out.println("| F-Score: " + df.format(evaluator.getFScore("triple")));
+    System.out.println("| MAP: " + df.format(evaluator.getMAP("triple")));
+    System.out.println("| GMAP: " + df.format(evaluator.getGMAP("triple")));
+    System.out.println("+------------------------------------+");
+    System.out.println("| Snippet                            ");
+    System.out.println("| Precision: " + df.format(evaluator.getPrecision("snippet")));
+    System.out.println("| Recall: " + df.format(evaluator.getRecall("snippet")));
+    System.out.println("| F-Score: " + df.format(evaluator.getFScore("snippet")));
+    System.out.println("| MAP: " + df.format(evaluator.getMAP("snippet")));
+    System.out.println("| GMAP: " + df.format(evaluator.getGMAP("snippet")));
+    System.out.println("+------------------------------------+");
   }
 
 }
--- project-team02/src/main/java/edu/cmu/lti/oaqa/pipeline/Evaluator.java
+++ project-team02/src/main/java/edu/cmu/lti/oaqa/pipeline/Evaluator.java
@@ -1,5 +1,6 @@
 package edu.cmu.lti.oaqa.pipeline;
 
+import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -7,8 +8,10 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import json.gson.Snippet;
 import edu.cmu.lti.oaqa.type.retrieval.ConceptSearchResult;
 import edu.cmu.lti.oaqa.type.retrieval.Document;
+import edu.cmu.lti.oaqa.type.retrieval.Passage;
 import edu.cmu.lti.oaqa.type.retrieval.TripleSearchResult;
 
 /**
@@ -31,14 +34,36 @@ public class Evaluator {
 
   private static int posTriple = 0;
 
-  // currently supposed answer number is golden answer number
+  // currently, we assume supposed answer number is golden answer number
   private static int supposeDoc = 0;
 
   private static int supposeConcept = 0;
 
   private static int supposeTriple = 0;
 
-  private static ArrayList<Double> avgPrecisionArr = new ArrayList<Double>();
+  // these variables are for snippets
+  private static int intersactSnippetNum = 0;
+
+  private static int snippetReturnNum = 0;
+
+  private static int snippetGoldNum = 0;
+
+  // 0: document; 1: concept; 2: triple; 3: snippet
+  private static ArrayList<ArrayList<Double>> avgPrecisionArr = new ArrayList<ArrayList<Double>>(4);
+
+  private static final int typeDocument = 0;
+
+  private static final int typeConcept = 1;
+
+  private static final int typeTriple = 2;
+
+  private static final int typeSnippet = 3;
+
+  Evaluator() {
+    for (int i = 0; i < 4; i++) {
+      avgPrecisionArr.add(new ArrayList<Double>());
+    }
+  }
 
   /**
    * Calculate positive number, total number, supposed number, and average precision of document
@@ -48,7 +73,8 @@ public class Evaluator {
    * @return
    */
   public int calDocPositive(Collection<Document> docList, List<String> docGold) {
-    if(docGold.size() == 0) return 0;
+    if (docGold.size() == 0)
+      return 0;
     Set<String> goldSet = new HashSet<String>();
 
     for (int i = 0; i < docGold.size(); i++) {
@@ -74,7 +100,7 @@ public class Evaluator {
     }
     System.out.println("| AVG Precision: " + avgPrecision);
     if (avgPrecision > 0.00001) {
-      avgPrecisionArr.add(avgPrecision);
+      avgPrecisionArr.get(typeDocument).add(avgPrecision);
     }
 
     totalDoc += docList.size();
@@ -94,7 +120,8 @@ public class Evaluator {
    */
   public int calConceptPositive(Collection<ConceptSearchResult> conceptList,
           List<String> conceptGold) {
-    if(conceptGold.size() == 0) return 0;
+    if (conceptGold.size() == 0)
+      return 0;
     Set<String> goldSet = new HashSet<String>();
 
     for (int i = 0; i < conceptGold.size(); i++) {
@@ -119,7 +146,7 @@ public class Evaluator {
     }
     System.out.println("| AVG Precision: " + avgPrecision);
     if (avgPrecision > 0.00001) {
-      avgPrecisionArr.add(avgPrecision);
+      avgPrecisionArr.get(typeConcept).add(avgPrecision);
     }
 
     totalConcept += conceptList.size();
@@ -137,8 +164,11 @@ public class Evaluator {
    * @param tripleGold
    * @return
    */
-  public int calTriplePositive(Collection<TripleSearchResult> tripleList, List<json.gson.Triple> tripleGold) {
-    if(tripleGold.size() == 0) return 0;
+  public int calTriplePositive(Collection<TripleSearchResult> tripleList,
+          List<json.gson.Triple> tripleGold) {
+    if (tripleGold.size() == 0)
+      return 0;
+    // store golden standard entry of three types
     HashMap<String, Integer> goldObjectDict = new HashMap<String, Integer>();
     ArrayList<String> goldPredicateArr = new ArrayList<String>();
     ArrayList<String> goldSubjectArr = new ArrayList<String>();
@@ -172,7 +202,7 @@ public class Evaluator {
     }
     System.out.println("| AVG Precision: " + avgPrecision);
     if (avgPrecision > 0.00001) {
-      avgPrecisionArr.add(avgPrecision);
+      avgPrecisionArr.get(typeTriple).add(avgPrecision);
     }
 
     totalTriple += tripleList.size();
@@ -183,15 +213,80 @@ public class Evaluator {
     return positiveNum;
   }
 
+  public int calSnippetPositive(Collection<Passage> snippetList, List<Snippet> snippetGold) {
+
+    // golden answer dictionary, key: start and end offset; value: frequency
+    HashMap<OffsetPair<Integer, Integer>, Integer> goldDict = new HashMap<OffsetPair<Integer, Integer>, Integer>();
+
+    for (int i = 0; i < snippetGold.size(); i++) {
+      Snippet snippet = snippetGold.get(i);
+      OffsetPair<Integer, Integer> thiskey = new OffsetPair<Integer, Integer>(
+              snippet.getOffsetInBeginSection(), snippet.getOffsetInEndSection());
+      if (goldDict.containsKey(thiskey)) {
+        int freq = goldDict.get(thiskey);
+        goldDict.put(thiskey, freq + 1);
+      } else {
+        goldDict.put(thiskey, 1);
+      }
+    }
+
+    double avgPrecision = 0.0;
+    int counter = 0;
+    int positiveNum = 0;
+    for (Passage p : snippetList) {
+      counter++;
+      OffsetPair<Integer, Integer> thiskey = new OffsetPair<Integer, Integer>(
+              p.getOffsetInBeginSection(), p.getOffsetInEndSection());
+
+      if (goldDict.containsKey(thiskey)) {
+        positiveNum++;
+        avgPrecision += (double) positiveNum / counter;
+      }
+    }
+    if (counter != 0) {
+      avgPrecision /= counter;
+    }
+    System.out.println("| AVG Precision: " + avgPrecision);
+    if (avgPrecision > 0.00001) {
+      avgPrecisionArr.get(typeSnippet).add(avgPrecision);
+    }
+
+    intersactSnippetNum += positiveNum;
+    snippetReturnNum += snippetList.size();
+    snippetGoldNum += snippetGold.size();
+
+    return positiveNum;
+  }
+
   /**
-   * Currently just sum up all three categories of result
+   * get precision value of certain type
    * 
+   * @param typeStr
+   *          : the type of data: document, concept, triple, snippet
    * @return
    */
-  public double getPrecision() {
-    if (totalDoc + totalConcept + totalTriple == 0)
-      return 0.0;
-    return (double) (posDoc + posConcept + posTriple) / (totalDoc + totalConcept + totalTriple);
+  public double getPrecision(String typeStr) {
+    switch (typeStr) {
+      case "document":
+        if (totalDoc == 0)
+          return 0.0;
+        return (double) posDoc / totalDoc;
+      case "concept":
+        if (totalConcept == 0)
+          return 0.0;
+        return (double) posConcept / totalConcept;
+      case "triple":
+        if (totalTriple == 0)
+          return 0.0;
+        return (double) posTriple / totalTriple;
+      case "snippet":
+        if (snippetReturnNum == 0)
+          return 0.0;
+        return (double) intersactSnippetNum / snippetReturnNum;
+      default:
+        System.err.println("Wrong type parameter for precision");
+        return 0.0;
+    }
   }
 
   /**
@@ -199,17 +294,37 @@ public class Evaluator {
    * 
    * @return
    */
-  public double getRecall() {
-    if (supposeDoc + supposeConcept + supposeTriple == 0)
-      return 0.0;
-    return (double) (posDoc + posConcept + posTriple)
-            / (supposeDoc + supposeConcept + supposeTriple);
+  public double getRecall(String typeStr) {
+    switch (typeStr) {
+      case "document":
+        if (supposeDoc == 0)
+          return 0.0;
+        return (double) posDoc / supposeDoc;
+      case "concept":
+        if (supposeConcept == 0)
+          return 0.0;
+        return (double) posConcept / supposeConcept;
+      case "triple":
+        if (supposeTriple == 0)
+          return 0.0;
+        return (double) posTriple / supposeTriple;
+      case "snippet":
+        if (snippetGoldNum == 0)
+          return 0.0;
+        return (double) intersactSnippetNum / snippetGoldNum;
+      default:
+        System.err.println("Wrong type parameter for recall");
+        return 0.0;
+    }
   }
 
-  public double getFScore() {
-    double precision = getPrecision();
-    double recall = getRecall();
-    return 2.0 * precision * recall / (precision + recall);
+  public double getFScore(String typeStr) {
+    double precision = getPrecision(typeStr);
+    double recall = getRecall(typeStr);
+    if (precision + recall == 0)
+      return 0.0;
+    else
+      return 2.0 * precision * recall / (precision + recall);
   }
 
   /**
@@ -217,17 +332,41 @@ public class Evaluator {
    * 
    * @return
    */
-  public double getMAP() {
+  public double getMAP(String typeStr) {
     double map = 0.0;
     double eplison = 0.00001; // to avoid zero
     int counter = 0;
-    for (int i = 0; i < avgPrecisionArr.size(); i++) {
-      if (avgPrecisionArr.get(i) > eplison) {
-        map += avgPrecisionArr.get(i);
+    int type = 0;
+
+    switch (typeStr) {
+      case "document":
+        type = typeDocument;
+        break;
+      case "concept":
+        type = typeConcept;
+        break;
+      case "triple":
+        type = typeTriple;
+        break;
+      case "snippet":
+        type = typeSnippet;
+        break;
+      default:
+        System.err.println("Wrong type parameter for getMAP");
+        return 0.0;
+    }
+
+    ArrayList<Double> currentArr = avgPrecisionArr.get(type);
+    for (int i = 0; i < currentArr.size(); i++) {
+      if (currentArr.get(i) > eplison) {
+        map += currentArr.get(i);
         counter++;
       }
     }
-    return map / counter;
+    if (counter == 0)
+      return 0.0;
+    else
+      return map / counter;
   }
 
   /**
@@ -235,14 +374,95 @@ public class Evaluator {
    * 
    * @return
    */
-  public double getGMAP() {
+  public double getGMAP(String typeStr) {
     double eplison = 0.00001; // to avoid zero
     double gmap = 1.0;
-    for (int i = 0; i < avgPrecisionArr.size(); i++) {
-      if (avgPrecisionArr.get(i) > eplison) {
-        gmap *= (avgPrecisionArr.get(i));
+    int type = 0;
+
+    switch (typeStr) {
+      case "document":
+        type = typeDocument;
+        break;
+      case "concept":
+        type = typeConcept;
+        break;
+      case "triple":
+        type = typeTriple;
+        break;
+      case "snippet":
+        type = typeSnippet;
+        break;
+      default:
+        System.err.println("Wrong type parameter for getMAP");
+        return 0.0;
+    }
+
+    ArrayList<Double> currentArr = avgPrecisionArr.get(type);
+    for (int i = 0; i < currentArr.size(); i++) {
+      if (currentArr.get(i) > eplison) {
+        gmap *= (currentArr.get(i));
       }
     }
-    return Math.pow(gmap, 1.0 / avgPrecisionArr.size());
+    // if gmap not change
+    if (gmap == 1.0)
+      return 0.0;
+    else
+      return Math.pow(gmap, 1.0 / avgPrecisionArr.size());
+  }
+
+  /**
+   * This class is used to store the start and end offset in snippet
+   * 
+   * @author yifu
+   *
+   * @param <A>
+   * @param <B>
+   */
+  private class OffsetPair<A, B> {
+    private A first;
+
+    private B second;
+
+    public OffsetPair(A first, B second) {
+      super();
+      this.first = first;
+      this.second = second;
+    }
+
+    public int hashCode() {
+      int hashFirst = first != null ? first.hashCode() : 0;
+      int hashSecond = second != null ? second.hashCode() : 0;
+      return (hashFirst + hashSecond) * hashSecond + hashFirst;
+    }
+
+    public boolean equals(Object other) {
+      if (other instanceof OffsetPair) {
+        OffsetPair otherPair = (OffsetPair) other;
+        return ((this.first == otherPair.first || (this.first != null && otherPair.first != null && this.first
+                .equals(otherPair.first))) && (this.second == otherPair.second || (this.second != null
+                && otherPair.second != null && this.second.equals(otherPair.second))));
+      }
+      return false;
+    }
+
+    public String toString() {
+      return "(" + first + ", " + second + ")";
+    }
+
+    public A getFirst() {
+      return first;
+    }
+
+    public void setFirst(A first) {
+      this.first = first;
+    }
+
+    public B getSecond() {
+      return second;
+    }
+
+    public void setSecond(B second) {
+      this.second = second;
+    }
   }
 }
